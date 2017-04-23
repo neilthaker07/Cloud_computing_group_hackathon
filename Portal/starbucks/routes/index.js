97ef58eb916232e4041f6d4c352dfa16d7ea47c2
Neil Thaker <neilthaker07@gmail.com>
2017-04-23 15:10:11 -0700

    Route request to KONG API

--- Portal/starbucks/routes/index.js
+++ Portal/starbucks/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Starbucks Order' });
+});
+
+router.post('/order', function(req, res, next) {
+    var store = req.body.store;
+    var items = req.body.items;
+    var kongIP = 'http://54.67.94.239:8000'
+
 });
 
 module.exports = router;
