06978ee16221f1f4ba5732a19864df606c387ca1
dishasheth <dsheth18@gmail.com>
2017-04-23 21:16:34 -0700

    DELETE Request

--- NodeBackend/app.js
+++ NodeBackend/app.js
@@ -36,7 +36,7 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/getOrders1',routes.getAllOrders);
 app.get('/getOrders',routes.getOrders);
-
+app.get('/deleteOrder',routes.deleteOrder);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- NodeBackend/routes/index.js
+++ NodeBackend/routes/index.js
@@ -4,9 +4,52 @@
  */
 var mongo = require("./mongo");
 var mongoURL = "mongodb://localhost:27017/Restbucks";
+var mongow = require('mongodb-wrapper')
 
 exports.index = function(req, res){
     res.render('index', { title:'HI'});
+};
+
+exports.deleteOrder = function(req, res){
+    console.log(req.param('id'));
+
+    mongo.connect(mongoURL, function(){
+
+        console.log('Connected to mongo at: ' + mongoURL);
+
+        var coll = mongo.collection('order');
+
+
+        var x=mongow.ObjectID(req.param('id'));
+        coll.remove({_id:x}, function(err, user){
+
+            if (user) {
+
+                // This way subsequent requests will know the user is logged in.
+
+                /*req.session.username = user.username;*/
+                //console.log(user.length +" is the session");
+
+                //json_res = {"statusCode" : 200};
+
+
+
+            } else {
+
+                console.log("returned false login");
+
+                //json_res = {"statusCode" : 401};
+
+            }
+
+          //  res.send(json_res);
+
+        });
+
+
+
+    });
+    res.redirect('/getOrders');
 };
 
 exports.getOrders = function(req,res) {
