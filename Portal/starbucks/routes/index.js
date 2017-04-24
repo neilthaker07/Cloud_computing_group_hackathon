a5c02afc36a1de6ed0b91234ec88e510ca5bd0a8
Neil Thaker <neilthaker07@gmail.com>
2017-04-23 20:33:09 -0700

    Code correction

--- Portal/starbucks/routes/index.js
+++ Portal/starbucks/routes/index.js
@@ -7,9 +7,40 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/order', function(req, res, next) {
-    var store = req.body.store;
-    var items = req.body.items;
     var kongIP = 'http://54.67.94.239:8000'
+    //req.redirect(kongIP)
+
+
+	var http = require('http');
+	var data = JSON.stringify({
+		'location': 'SF',
+		'items': {'name':'first', 'size':'5'}
+	});
+
+	var options = {
+	  host: '54.67.94.239',
+	  port: '8000',
+	  method: 'GET',
+	  headers: {
+	    'Content-Type': 'application/json; charset=utf-8',
+	    'Content-Length': data.length
+	  }
+	};
+
+	  var req1 = http.request(options, function(res) {
+	  var msg = '';
+
+	  res.setEncoding('utf8');
+	  res.on('data', function(chunk) {
+	    msg += chunk;
+	  });
+	  res.on('end', function() {
+	    console.log(JSON.parse(msg));
+	  });
+	});
+
+	req1.write(data);
+	req1.end();
 
 });
 
