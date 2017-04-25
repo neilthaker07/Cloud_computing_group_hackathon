d7fe012c6177fe1ddbd8bf1dcfcd0d93b1453490
Neil Thaker <neilthaker07@gmail.com>
2017-04-24 18:11:38 -0700

    Connection to kong api from node js

--- Portal/starbucks/routes/index.js
+++ Portal/starbucks/routes/index.js
@@ -10,38 +10,36 @@ router.post('/order', function(req, res, next) {
     var kongIP = 'http://54.67.94.239:8000'
     //req.redirect(kongIP)
 
-
 	var http = require('http');
-	var data = JSON.stringify({
-		'location': 'SF',
-		'items': {'name':'first', 'size':'5'}
+
+	var dict = [];
+	dict.push({
+    	key: "Host",
+    	value: "pa"
 	});
 
 	var options = {
-	  host: '54.67.94.239',
-	  port: '8000',
-	  method: 'GET',
-	  headers: {
-	    'Content-Type': 'application/json; charset=utf-8',
-	    'Content-Length': data.length
-	  }
+	hostname: '54.67.94.239',
+	port: '8000',
+	method: 'GET',
+	headers: dict
 	};
 
-	  var req1 = http.request(options, function(res) {
-	  var msg = '';
+	callback = function(response) {
+	  var str = '';
 
-	  res.setEncoding('utf8');
-	  res.on('data', function(chunk) {
-	    msg += chunk;
+	  //another chunk of data has been recieved, so append it to `str`
+	  response.on('data', function (chunk) {
+	    str += chunk;
 	  });
-	  res.on('end', function() {
-	    console.log(JSON.parse(msg));
+
+	  //the whole response has been recieved, so we just print it out here
+	  response.on('end', function () {
+	    console.log(str);
 	  });
-	});
+	}
 
-	req1.write(data);
-	req1.end();
-
+	http.request(options, callback).end();
 });
 
 module.exports = router;
