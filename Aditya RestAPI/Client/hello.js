a231c11dc28ec0cfb08b06bc8bdbccaa29027bae
Aditya Parmar <parmar415@gmail.com>
2017-05-06 17:21:40 -0700

    UI for API Testing for CORS

--- Aditya RestAPI/Client/hello.js
+++
@@ -1,8 +0,0 @@
-function UserAction() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "Your Rest URL Here", false);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
-    var response = JSON.parse(xhttp.responseText);
-    console.log(response);
-}
